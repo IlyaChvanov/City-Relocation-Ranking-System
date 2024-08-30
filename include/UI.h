1193a0baf3b74244dc3c57c874982c2da79828e0
Ilya Chvanov <ivchvanov@edu.hse.ru>
2024-08-30 13:49:04 +0300

    implement UI

--- include/UI.h
+++ include/UI.h
@@ -1,8 +1,21 @@
 #ifndef COUNTRY_RELOCATION_RANKING_SYSTEM__UI_H_
 #define COUNTRY_RELOCATION_RANKING_SYSTEM__UI_H_
 
-class UI {
+#include <vector>
+#include <memory>
+#include <iostream>
 
+#include "country.h"
+#include "i_db_manager.h"
+
+//TODO Дописать тест на Print, когда появится БД
+using DB = std::shared_ptr<const IDBManager>;
+class UI {
+ public:
+  explicit UI(DB  db);
+  void PrintTopCountries(size_t countries_num) const;//takes from DB
+ private:
+  const DB db_;
 };
 
 #endif //COUNTRY_RELOCATION_RANKING_SYSTEM__UI_H_
--- src/UI.cpp
+++ src/UI.cpp
@@ -1,1 +1,16 @@
 #include "UI.h"
+
+#include <utility>
+#include "i_db_manager.h"
+
+using std::cout;
+void UI::PrintTopCountries(size_t countries_num) const {
+  for (const auto& country : db_->LoadCountries(countries_num)) {
+    cout << country.rating_position << ": " << "\033[1m" << country.name << "\033[1m" << '\n'
+    << "climate: " << country.points_climate << '\n'
+    << "life quality: " << country.points_life_quality << '\n'
+    << "language complexity: " << country.points_language << '\n'
+    << "_______________________________" << '\n';
+  }
+}
+UI::UI(DB  db) : db_(std::move(db)) {}
