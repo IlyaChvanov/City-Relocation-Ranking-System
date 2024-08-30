aa391f7fb90191ff74186f00cd4700487fde4c72
Ilya Chvanov <ivchvanov@edu.hse.ru>
2024-08-30 12:42:14 +0300

    implemented country.h

--- include/country.h
+++ include/country.h
@@ -1,8 +1,14 @@
 #ifndef COUNTRY_RELOCATION_RANKING_SYSTEM_INCLUDE_COUNTRY_H_
 #define COUNTRY_RELOCATION_RANKING_SYSTEM_INCLUDE_COUNTRY_H_
 
-class Country {
+#include <string>
 
+struct Country {
+  std::string name;
+  double points_climate;
+  double points_life_quality;
+  double points_language;
+  int rating_position;
 };
 
 #endif //COUNTRY_RELOCATION_RANKING_SYSTEM_INCLUDE_COUNTRY_H_
