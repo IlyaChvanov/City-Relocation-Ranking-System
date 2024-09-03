9ca33b6f4e3c4ddcb4f0cf7724174560f19e5dc6
Ilya Chvanov <ivchvanov@edu.hse.ru>
2024-09-03 13:04:32 +0300

    implementation i_data_collector.h

--- include/i_data_collector.h
+++ include/i_data_collector.h
@@ -1,8 +1,22 @@
 #ifndef COUNTRY_RELOCATION_RANKING_SYSTEM__I_DATA_COLLECTOR_H_
 #define COUNTRY_RELOCATION_RANKING_SYSTEM__I_DATA_COLLECTOR_H_
 
-class IDataCollector {
+#include <string_view>
 
+#include "nlohmann/json.hpp"
+
+
+class IDataCollector {
+ public:
+  virtual void FetchData(const std::string& city) = 0;
+  virtual std::pair<std::string, std::string> GetParsedData() = 0;
+  virtual ~IDataCollector() = default;
+ protected:
+  virtual void ParseData(const nlohmann::json& json) = 0;
+  nlohmann::json PerformGetRequest(const std::string& host,
+                                   const std::string& target) const;
 };
+
+
 
 #endif //COUNTRY_RELOCATION_RANKING_SYSTEM__I_DATA_COLLECTOR_H_
