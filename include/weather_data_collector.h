24c92f70e8f62b1739df80bc0910520b592228e2
Ilya Chvanov <ivchvanov@edu.hse.ru>
2024-09-03 13:06:30 +0300

    function for making coordinates from city name

--- include/weather_data_collector.h
+++ include/weather_data_collector.h
@@ -3,6 +3,22 @@
 
 class WeatherDataCollector {
 
+struct CityCoord {
+  friend std::ostream& operator<<(std::ostream& os, const CityCoord& coord);
+  double lat;
+  double lon;
+};
+
+
+class WeatherDataCollector : public IDataCollector {
+ public:
+  void FetchData(const std::string& city) override;
+  std::pair<std::string, std::string> GetParsedData() override;
+  CityCoord CoordFromName(const std::string& city);
+ private:
+  void ParseData(const nlohmann::json& json) override;
+  std::pair<std::string, std::string> parsed_data_;
+  const std::string open_weather_api_key_ = "457b2a41705a1c5415360df4a71b8d59";
 };
 
 #endif //COUNTRY_RELOCATION_RANKING_SYSTEM_INCLUDE_WEATHER_DATA_COLLECTOR_H_
--- src/weather_data_collector.cpp
+++ src/weather_data_collector.cpp
@@ -1,1 +1,38 @@
 #include "weather_data_collector.h"
+
+#include <boost/beast/http.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <nlohmann/json.hpp>
+#include <string>
+#include <iostream>
+
+namespace beast = boost::beast;
+namespace http = beast::http;
+namespace net = boost::asio;
+using tcp = net::ip::tcp;
+
+void WeatherDataCollector::FetchData(const std::string& city) {
+}
+
+std::pair<std::string, std::string> WeatherDataCollector::GetParsedData() {
+
+}
+void WeatherDataCollector::ParseData(const nlohmann::json& json) {
+
+}
+
+CityCoord WeatherDataCollector::CoordFromName(const std::string& city) {
+  std::string host = "api.openweathermap.org";
+  std::string target = "/geo/1.0/direct?q=" + city +
+                       "&limit=1&appid=" + open_weather_api_key_;
+  auto json = PerformGetRequest(host, target);
+  double lat = json[0].at("lat");
+  double lon = json[0].at("lon");
+  return {lat, lon};
+}
+std::ostream& operator<<(std::ostream& os, const CityCoord& coord) {
+  os << "CityCoord(" << coord.lat << ", " << coord.lon << ")";
+  return os;
+}
+
+
